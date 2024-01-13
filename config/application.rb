eae5287dfac44c81278ba4b58f8213f177a48dc4
Tim Morgan <github@timothymorgan.info>
2024-01-12 16:45:41 -0800

    Ruby 3.3; Rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,11 @@ module Ranked
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -37,6 +42,8 @@ module Ranked
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_record.encryption.support_sha1_for_non_deterministic_encryption
 
     config.generators do |g|
       g.test_framework      :rspec, fixture: true, views: false
