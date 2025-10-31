1c948bdadbafcc6ab6ceb432226eb8730ff8118f
Tim Morgan <github@timothymorgan.info>
2025-10-31 00:47:49 -0700

    Rails and gem updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2) do
+ActiveRecord::Schema[8.1].define(version: 2) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "cards", force: :cascade do |t|
-    t.bigint "stack_id", null: false
     t.string "name", limit: 126, null: false
+    t.bigint "stack_id", null: false
     t.index ["stack_id", "name"], name: "index_cards_on_stack_id_and_name", unique: true
     t.index ["stack_id"], name: "index_cards_on_stack_id"
   end
 
   create_table "stacks", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
     t.string "name", limit: 126, null: false
     t.text "pairs_order", null: false
-    t.datetime "created_at", precision: nil, null: false
   end
 
   add_foreign_key "cards", "stacks", on_delete: :cascade
